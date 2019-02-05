8044a1faede170fc4e8b3fea50f8fca34c0304fe
IgorKozerski <ikspun@gmail.com>
2019-02-05 13:27:49 +0000

    tests and find word start

---
+++ core/src/com/knowledgemagnet/detektywslow/game_objects/FindedWordPosition.java
@@ -0,0 +1,8 @@
+package com.knowledgemagnet.detektywslow.game_objects;
+
+/**
+ * Created by Igor on 01.02.2019.
+ */
+
+public class findedWordPosition {
+}
---
+++ core/tests/com/knowledgemagnet/detektywslow/game_objects/buttons/PositionFinderTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by Igor on 31.01.2019.
+ */
+public class PositionFinderTest {
+
+}
